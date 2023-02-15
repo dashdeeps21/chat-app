63753d9565ddda0c0765696670ee84b8ccd9acd3
Deepak kadela <deepakkadela21@gmail.com>
2023-02-15 18:08:12 +0530

    Project Completed

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Cam from "../Images/cam.png";
 import Add from "../Images/add.png";
 import More from "../Images/more.png";
 import Messages from "./Messages";
-import Input from "./input";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
+  const { data } = useContext(ChatContext);
+
   return (
     <div className="chat">
       <div className="chatInfo">
-        <span>Jane</span>
+        <span>{data.user?.displayName}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
